bddd49c19fc2bb9409ae479dd434dfae7f378fa3
Ermiyas-T <jermiket@gmail.com>
2026-03-17 22:32:19 +0300

    define types that mirror the backend (permision ) for consistency

---
+++ client/types/index.ts
@@ -0,0 +1,34 @@
+// types/index.ts
+export type Action = "create" | "read" | "update" | "delete" | "manage";
+export type Subject =
+  | "User"
+  | "Role"
+  | "Permission"
+  | "Post"
+  | "Comment"
+  | "all";
+
+export interface User {
+  id: string;
+  email: string;
+}
+
+export interface LoginResponse {
+  token: string;
+  user: User;
+  permissions: string[]; // e.g., ["create:Post", "read:User"]
+}
+
+export interface Role {
+  id: string;
+  name: string;
+  description?: string;
+  permissions?: Permission[];
+}
+
+export interface Permission {
+  id: string;
+  action: string;
+  subject: string;
+  conditions?: unknown;
+}
